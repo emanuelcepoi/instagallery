d62c15a3be888b6fec9c20bb2715fa76f4f9c74e
Emanuel Cepoi <emanuelcepoi@gmail.com>
2019-04-14 00:38:58 +0300

    Created basic photogrid files structure

---
+++ src/components/photoGrid.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+class PhotoGrid extends Component {
+    render() {
+      return (
+        <View>
+            <Text>Hello from my photo grid</Text>
+        </View>
+      );
+    }
+  }
+
+  const styles = StyleSheet.create({
+
+  })
+
+
+  export default PhotoGrid
--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import Header from '../components/header';
+import PhotoGrid from '../components/photoGrid';
 
 class HomeScreen extends Component {
     render() {
       return (
         <View style={styles.container}>
          <Header headerTitle={'instagram'} />
+         <PhotoGrid />
         </View>
       );
     }
