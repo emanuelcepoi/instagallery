1212b0f66b1fa38f393ab4357df8b6ba4bb4b5c0
Emanuel Cepoi <emanuelcepoi@gmail.com>
2019-04-14 05:06:58 +0300

    Displaying swipable area starting from pressed image

--- src/components/previewModal.js
+++ src/components/previewModal.js
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Modal, TouchableHighlight } from 'react-native';
-import {connect} from 'react-redux'
+import { StyleSheet, Text, View, Modal, TouchableHighlight, Image } from 'react-native';
+import { connect } from 'react-redux'
+
+import Swiper from 'react-native-swiper';
+
 class PreviewModal extends Component {
     constructor() {
         super();
+        this.state = {
+            description: null,
+            title: null
+        }
 
     }
+
     render() {
         return (
             <Modal
                 animationType="slide"
                 transparent={true}
                 visible={this.props.modalState}
-               >
+            >
                 <View style={styles.modalBackground}>
-                    <View>
-                        <Text>{this.props.data[Number(this.props.pressedImage)].title}</Text>
-
+                    <View style={styles.container}>
+                        <Swiper showsButtons={false} 
+                                showsPagination={false} 
+                                index={this.props.data[Number(this.props.pressedImage)].id}
+                                // onIndexChanged={ (index) => alert(index)}
+                                >
+                            {this.props.data.map((data, index) => {
+                                return (
+                                    <View style={styles.wrapper}>
+                                        <Image
+                                            style={{ width: '100%', height: 300 }}
+                                            source={{ uri: this.props.data[index].src }}
+                                        />
+                                    </View>
+                                )
+                            })}
+                        </Swiper>
                         <TouchableHighlight
-                            onPress={this.props.hideModal }>
+                            onPress={this.props.hideModal}>
                             <Text>Hide Modal</Text>
                         </TouchableHighlight>
                     </View>
@@ -35,6 +57,14 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         justifyContent: 'space-around',
         backgroundColor: 'rgba(0, 0, 0, 0.5)'
+    },
+    container: {
+        height: 300,
+        width: '100%'
+    },
+    wrapper: {
+        width: '100%',
+        height: 300
     }
 })
 
@@ -47,7 +77,7 @@ const mapStateToProps = (state) => {
 
 function mapDispatchToProps(dispatch) {
     return {
-        hideModal: () => dispatch({type: 'HIDE_MODAL'})
+        hideModal: () => dispatch({ type: 'HIDE_MODAL' })
     }
 }
 
